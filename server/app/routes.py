6e7e82cc5f42943759052a398224e4d383b9f998
Anton Chekashev <not.antony@yandex.ru>
2020-05-16 20:17:24 +0300

    Remove unused directories

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,4 +1,3 @@
-import tempfile
 import base64
 import json
 import flask
@@ -21,44 +20,41 @@ def index():
 
 @app.route('/detection', methods=['POST'])
 def detection():
-    with tempfile.TemporaryDirectory("box-session") as tmpdir:
-        if request.mimetype == "image/jpeg" or request.mimetype == "image/png":
-            image_data = request.get_data()
-        elif request.mimetype == "application/json":
-            json_data = json.loads(request.get_data())
-            image_data = base64.decodebytes(json_data["data"].encode())
-            _extension = json_data["type"]
-        else:
-            return "Unsupported MIME type: `{}`".format(request.mimetype)
+    if request.mimetype == "image/jpeg" or request.mimetype == "image/png":
+        image_data = request.get_data()
+    elif request.mimetype == "application/json":
+        json_data = json.loads(request.get_data())
+        image_data = base64.decodebytes(json_data["data"].encode())
+        _extension = json_data["type"]
+    else:
+        return "Unsupported MIME type: `{}`".format(request.mimetype)
 
-        return flask.jsonify(google_query.get_boxes(image_data))
+    return flask.jsonify(google_query.get_boxes(image_data))
 
 
-# TODO: refactor processing
 @app.route('/crop-gcloud', methods=['POST'])
 def crop_gcloud():
-    with tempfile.TemporaryDirectory("crop-session") as tmpdir:
-        aspect_ratio = 3/4
-        if request.mimetype == "image/jpeg" or request.mimetype == "image/png":
-            image_data = request.get_data()
-        elif request.mimetype == "application/json":
-            json_data = json.loads(request.get_data())
-            image_data = base64.b64decode(json_data["data"])
-            _extension = json_data["type"]
-            if "aspect_ratio" in json_data:
-                aspect_ratio_str = json_data["aspect_ratio"]
-                try:
-                    match = re.match(r"(\d+)/(\d+)", aspect_ratio_str)
-                    if match:
-                        aspect_ratio = float(match.group(1)) / float(match.group(2))
-                    else:
-                        aspect_ratio = float(aspect_ratio_str)
-                except ValueError:
-                    return "Cannot parse aspect_ratio value: `{}`".format(aspect_ratio_str)
-        else:
-            return "Unsupported MIME type: `{}`".format(request.mimetype)
+    aspect_ratio = 3/4
+    if request.mimetype == "image/jpeg" or request.mimetype == "image/png":
+        image_data = request.get_data()
+    elif request.mimetype == "application/json":
+        json_data = json.loads(request.get_data())
+        image_data = base64.b64decode(json_data["data"])
+        _extension = json_data["type"]
+        if "aspect_ratio" in json_data:
+            aspect_ratio_str = json_data["aspect_ratio"]
+            try:
+                match = re.match(r"(\d+)/(\d+)", aspect_ratio_str)
+                if match:
+                    aspect_ratio = float(match.group(1)) / float(match.group(2))
+                else:
+                    aspect_ratio = float(aspect_ratio_str)
+            except ValueError:
+                return "Cannot parse aspect_ratio value: `{}`".format(aspect_ratio_str)
+    else:
+        return "Unsupported MIME type: `{}`".format(request.mimetype)
         
-        return flask.jsonify(google_query.get_crops(image_data, aspect_ratio))
+    return flask.jsonify(google_query.get_crops(image_data, aspect_ratio))
 
 
 # @app.route('/bgrm', methods=['POST'])
